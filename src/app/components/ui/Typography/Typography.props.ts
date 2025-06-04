42e28e42b58d1c782460f9461031a94e400ca927
Pavel <Pav_kaf@mail.ru>
2025-06-04 17:22:40 +0300

    add typography component

---
+++ src/app/components/ui/Typography/Typography.props.ts
@@ -0,0 +1,12 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+type TextTags = "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span";
+
+export default interface TypographyProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+  as?: TextTags;
+  children: React.ReactNode;
+  color: "black" | "white";
+  className?: string;
+  variants: "large" | "medium" | "main";
+}
---
+++ src/app/components/ui/Typography/Typography.tsx
@@ -0,0 +1,25 @@
+import styles from "./styles.module.css";
+import clsx from "clsx";
+import TypographyProps from "./Typography.props";
+
+export default function Typography({
+  as: Tag = "p",
+  variants,
+  children,
+  className,
+  color,
+}: TypographyProps) {
+  return (
+    <Tag
+      className={clsx(styles.text, className, {
+        [styles.black]: color === "black",
+        [styles.white]: color === "white",
+        [styles.large]: variants === "large",
+        [styles.medium]: variants === "medium",
+        [styles.main]: variants === "main",
+      })}
+    >
+      {children}
+    </Tag>
+  );
+}
